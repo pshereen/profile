22ffad5f3647e9eba5441c2c6187a2571aed1588
Shereen Punnassery <pshereen@gmail.com>
2025-04-20 19:17:22 -0700

    Added Uber Eats project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import rockPaperImage from '../assets/Rock-paper-scissors.png';
 import didYouKnowImage from '../assets/DidYouKnow.jpg';
+import ubereats from '../assets/UberEats.png';
 import reactLogo from '../assets/react.svg';
 import typescriptLogo from '../assets/TypeScript.svg';
 import tailwindLogo from '../assets/Tailwind CSS.svg';
@@ -11,6 +12,8 @@ export default function Projects() {
       <h2 className="text-4xl font-extrabold text-center mb-12">Projects</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-7xl mx-auto">
+
+        {/* Rock Paper Scissors Game */}
 
         <div className="bg-white rounded-2xl p-4 md:p-6 shadow-lg flex flex-row items-center gap-6">
           <img
@@ -60,6 +63,8 @@ export default function Projects() {
           </div>
         </div>
 
+        {/* Did You Know Quiz App */}
+
         <div className="bg-white rounded-2xl p-4 md:p-6 shadow-lg flex flex-row items-center gap-6">
           <img
             src={didYouKnowImage}
@@ -106,6 +111,56 @@ export default function Projects() {
             </div>
           </div>
         </div>
+
+        {/* Uber Eats Clone */}
+
+        <div className="bg-white rounded-2xl p-4 md:p-6 shadow-lg flex flex-row items-center gap-6">
+          <img
+            src={ubereats}
+            alt="Quiz App"
+            className="w-40 h-32 rounded-md object-cover"
+          />
+
+          <div className="flex flex-col justify-between gap-2 flex-1">
+            <h3 className="text-xl font-bold text-[#4338ca]">Uber Eats</h3>
+            <p className="text-gray-700 text-sm">
+            A full-stack Uber Eats clone app hosted on Render, allowing customers to browse restaurants, add items to a cart, and place orders. 
+            Restaurant owners can manage menus and track orders through a dedicated dashboard with real-time updates.
+            </p>
+            <div className="flex justify-between items-center mt-2">
+              <div className="flex items-center gap-3">
+                <a
+                  href="https://pshereen.github.io/uber-eats"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-2xl cursor-pointer hover:opacity-80"
+                  title="Live Site"
+                >
+                  🌐
+                </a>
+                <a
+                  href="https://github.com/pshereen/uber-eats"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={githubIcon}
+                    alt="GitHub"
+                    className="w-6 h-6 cursor-pointer hover:opacity-80"
+                    title="View on GitHub"
+                  />
+                </a>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <img src={reactLogo} alt="React" className="w-6 h-6" title="React" />
+                <img src={typescriptLogo} alt="TypeScript" className="w-6 h-6" title="TypeScript" />
+                <img src={tailwindLogo} alt="Tailwind CSS" className="w-6 h-6" title="Tailwind CSS" />
+              </div>
+            </div>
+          </div>
+        </div>
+      
       </div>
     </section>
   );
